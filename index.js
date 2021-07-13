e756bfa48d8850488069e98ee03bd31ff2fc6418
Alana Caporale <alanacaporale@gmail.com>
2021-07-12 20:53:33 -0400

    added post functionality

--- index.js
+++ index.js
@@ -17,11 +17,31 @@ const dogs = [
         age: "9 months old"
     }
 ]
+
+const posts = [
+    {
+        image: "img/gracie_grass.jpg",
+        username: "@CrazyGracie123",
+        likes: 2,
+        postDate: "1 hour ago"
+    },
+    {
+        image: "img/gracie_grass.jpg",
+        username: "@StellaGirlxoxo",
+        likes: 367,
+        postDate: "07/11/21"
+    },
+    {
+        image: "img/gracie_grass.jpg",
+        username: "@CharlotteTheQueen",
+        likes: 99,
+        postDate: "Today"
+    }
+]
 function handleclick(){
     console.log("clicked")
 }
 function renderProfiles(){
-
     for (let i = 0; i < dogs.length; i++) {
         const profile = document.createElement("div")
         const profileImg = document.createElement("img")
@@ -46,8 +66,41 @@ function renderProfiles(){
 
         dogBasket.append(profile)
     }
-    
+  }
+const recentPosts = document.getElementById("posts")
+function renderPosts(){
+    for (let i = 0; i < posts.length; i++) {
+        const postCard = document.createElement("div")
+        const postImg = document.createElement("img")
+        const username = document.createElement("p")
+        const postDate = document.createElement("p")
+        const likesBtn = document.createElement("button")
+        const likes = document.createElement('p')
+        postCard.setAttribute("class","post-style")
+        postImg.setAttribute("class","post-card-img")
+        username.style.display="block"
+        likesBtn.style.float ="left"
+        likes.style.float="left"
+        likesBtn.style.fontFamily='Material Icons'
+        likesBtn.style.padding="10px 10px"
+        postDate.style.float="right"
 
+        postImg.src = posts[i].image
+        username.textContent = posts[i].username
+        postDate.textContent = posts[i].postDate
+        likesBtn.innerHTML = `<span class="material-icons-outlined">
+        pets</span>`
+        likes.textContent = `${posts[i].likes} likes`
+
+
+        postCard.append(postImg)
+        postCard.append(username)
+        postCard.append(postDate)
+        postCard.append(likesBtn)
+        postCard.append(likes)
+        recentPosts.append(postCard)
+    }
 }
 
 renderProfiles()
+renderPosts()
